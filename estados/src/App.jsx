b63b09b6620206e9359f415c45941a5c223b0400
WILLIAM DA SILVA ALVES <william_s_alves@estudante.sesisenai.org.br>
2025-02-26 22:18:25 -0300

    aula de ontem

--- estados/src/App.jsx
+++ estados/src/App.jsx
@@ -1,11 +1,13 @@
 
   import { useState } from 'react'
   import './App.css'
+import Conversao from './components/Conversao';
+import Conversao2 from './components/Conversao2';
 
   function App() {
     const [usuario, setUsuario] = useState("Juca ");
     const [nome, setNome] = useState("Juca");
-
+    const [senha,setSenha] = useState ("senha");
 
   function lernome(){
     let nome = prompt("Digite seu nome")
@@ -14,13 +16,24 @@
     function lerUsuario(){
       let resposta = prompt("digite o novo usuario?")
       setUsuario(resposta);
+      let pw=  prompt('Digite sua nova senha :')
+      let pw2= prompt ('confirme sua senha:')
+      if(pw==pw2){
+        setSenha (pw)
+      }else {
+        alert ("as senhas sao diferentes")
+      }
+
     }
     return (
       <>
+       <Conversao2/>
+      <Conversao /> 
         <h1>Estados</h1>
         Nome: {nome}
         <div>
         Usuário: {usuario}
+        senha: {senha}
         </div>
         <button onClick={lernome}>Alterar nome</button>
         <button onClick={lerUsuario}> Trocar Usuario </button>
