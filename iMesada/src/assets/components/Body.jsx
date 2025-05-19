5d6f651156b97132a53f2e3c445a0770c6cc392d
Williamalves96 <165970735+Williamalves96@users.noreply.github.com>
2025-05-19 20:41:49 -0300

    por enquanto so esta funcionando o input de saldo   deb cred

--- iMesada/src/assets/components/Body.jsx
+++ iMesada/src/assets/components/Body.jsx
@@ -4,20 +4,49 @@ import "./Body.css";
 function Body() {
   const [inputValor, setinputValor] = useState("");
   const [saldo, setSaldo] = useState(0);
+  const [inputTransacao, setInputTransacao] = useState("");
+  const [movimentacoes, setMovimentacoes] = useState([]);
 
+  function creditar() {
+    let transacao = {
+      id: Date.now(),
+      movimentacao: inputTransacao,
+      valor: Number(inputValor),
+      tipo: "credito",
+    };
+    setMovimentacoes([transacao, ...movimentacoes]);
+    setSaldo(saldo + Number(inputValor));
+  }
+
+  function debitar() {
+    let transacao = {
+      id: Date.now(),
+      movimentacao: inputTransacao,
+      valor: Number(inputValor),
+      tipo: "debito",
+    };
+  }
   return (
     <div className="container-body">
       <h1> Controlinho Financeiro </h1>
-      R${saldo}
+      <h3> Saldo R$ {saldo}</h3>
+      <input
+        type="text"
+        placeholder="transação"
+        value={inputTransacao}
+        onChange={(event) => setInputTransacao(event.target.value)}
+      />{" "}
+      <br />
       <input
         type="Number"
         placeholder="valor"
         value={inputValor}
         onChange={(event) => setinputValor(event.target.value)}
-      />
+      />{" "}
+      <br />
       <div>
         <button
-          onClick={() => setSaldo(saldo - inputValor)}
+          onClick={() => setSaldo(saldo - Number(inputValor))}
           className="botao-debto"
         >
           {" "}
@@ -30,6 +59,10 @@ function Body() {
           {" "}
           Credit{" "}
         </button>
+      </div>
+      <div>
+        {creditar}
+        {debitar}
       </div>
     </div>
   );
